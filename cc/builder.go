5860f949abb3948014d5cdaa2f5e09d72cfd2ae4
Chih-Hung Hsieh <chh@google.com>
2018-06-04 10:37:43 -0700

    Use llvm-{objcopy,strip} when clang lld is used.
    * Pass LLVM_{OBJCOPY,STRIP} to makefile rules.
    * strip.sh is used only in soong-only mode.
      It will use llvm-strip and  and llvm-objcopy
      when --use-llvm-strip is given.
      Keep flags of strip.sh in alphabetic order.
    * Tested build/soong/scripts/build-ndk-prebuilts.sh
      by adding "UseClangLld": true, to soong.variables file.
    
    Bug: 80093681
    Test: build/soong/scripts/build-ndk-prebuilts.sh
    Change-Id: I612267304eea434c7a33cc086b27b577d5f64094

--- cc/builder.go
+++ cc/builder.go
@@ -116,8 +116,8 @@ var (
 		blueprint.RuleParams{
 			Depfile:     "${out}.d",
 			Deps:        blueprint.DepsGCC,
-			Command:     "CROSS_COMPILE=$crossCompile $stripPath ${args} -i ${in} -o ${out} -d ${out}.d",
-			CommandDeps: []string{"$stripPath"},
+			Command:     "CROSS_COMPILE=$crossCompile XZ=$xzCmd CLANG_BIN=${config.ClangBin} $stripPath ${args} -i ${in} -o ${out} -d ${out}.d",
+			CommandDeps: []string{"$stripPath", "$xzCmd"},
 		},
 		"args", "crossCompile")
 
@@ -255,8 +255,8 @@ type builderFlags struct {
 	stripKeepSymbols       bool
 	stripKeepMiniDebugInfo bool
 	stripAddGnuDebuglink   bool
-
 	quicksilver bool
+	stripUseLlvmStrip      bool
 }
 
 type Objects struct {
@@ -871,6 +871,9 @@ func TransformStrip(ctx android.ModuleContext, inputFile android.Path,
 	}
 	if flags.stripKeepSymbols {
 		args += " --keep-symbols"
+	}
+	if flags.stripUseLlvmStrip {
+		args += " --use-llvm-strip"
 	}
 
 	ctx.Build(pctx, android.BuildParams{
